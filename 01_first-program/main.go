4ccb6b7f720e44e50596c15ae587aecd7a8b6c2c
Dinithi Nanayakkara <166215353+2003dinijay@users.noreply.github.com>
2026-07-24 04:29:39 +0530

    add initial Go program with variable declarations and print statements

--- 01_first-program/main.go
+++ 01_first-program/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //main() is the entry point
 //go looks for package main and func main() to run the program
-
+// entry point is the main() function
 func main() {
 	fmt.Println("Hello Go")
 }
---
+++ 02_variables_and_types/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name type
+	var name string
+	name = "DINITHI"
+
+	var year int = 2003
+
+	fmt.Println("Name:", name)
+	fmt.Println("Year:", year)
+
+	//declare multiple variables at once
+
+	var a, b, c int = 1, 2, 3
+	fmt.Println("a:", a, "b:", b, "c:", c)
+}
